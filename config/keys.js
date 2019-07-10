f2f4b7ec6d6aa613baac57c59de93aa935c11d39
Monkey D. Barti <pierre.le-corff@epita.fr>
2019-07-10 11:37:44 +0200

    up-to-date

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+dbPassword =
+	"mongodb+srv://MonkeyDBarti:" +
+	encodeURIComponent("cvcreator2019") +
+	"@cvcreator-4jac9.mongodb.net/test?retryWrites=true&w=majority";
+
+module.exports = {
+	mongoURI: dbPassword
+};
