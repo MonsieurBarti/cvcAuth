684433b80f7317ce28ac34aecd986fcbb43e3682
Monsieur Barti <pierre.le-corff@epita.fr>
2019-09-12 16:47:38 +0200

    push pour pierrot

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 dbPassword =
 	"mongodb+srv://MonkeyDBarti:" +
 	encodeURIComponent("cvcreator2019") +
-	"@cvcreator-4jac9.mongodb.net/test?retryWrites=true&w=majority";
+	"@cvcreator-4jac9.mongodb.net/test?retryWrites=true&w=majority"
 
 module.exports = {
 	mongoURI: dbPassword
-};
+}
--- models/user.js
+++ models/user.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true
-	},
 	email: {
 		type: String,
 		required: true
 	},
 	password: {
 		type: String,
+		required: true
+	},
+	role: {
+		type: Number,
+		default: 1,
 		required: true
 	},
 	date: {
